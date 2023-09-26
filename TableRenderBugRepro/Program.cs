c6e96e6d9530e865cc78c7aca37ce3539dbb5dce
Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
2023-09-26 11:49:32 +0100

    Repro

---
+++ TableRenderBugRepro/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Spectre.Console;
+
+var table = new Table();
+
+table.AddColumn("Module");
+table.AddColumn("Duration");
+table.AddColumn("Status");
+table.AddColumn("Start");
+table.AddColumn("End");
+table.AddColumn(string.Empty);
+
+for (var i = 0; i < 55; i++)
+{
+    table.AddRow(
+        $"[cyan]{Guid.NewGuid():N}[/]",
+        $"{Guid.NewGuid():N}",
+        $"{Guid.NewGuid():N}",
+        $"{Guid.NewGuid():N}",
+        $"{Guid.NewGuid():N}",
+        $"{Guid.NewGuid():N}");
+            
+    table.AddEmptyRow();
+}
+
+Console.WriteLine();
+AnsiConsole.Write(table);
+Console.WriteLine();
